4e86093b822ae7a6342c2b1e44b56fdc5e42da65
Zaknarfen <ad.renan@gmail.com>
2017-08-23 14:00:02 -0300

    Adding export to routes
    	- Added an export to the routes array
    	- Added the routes export also in index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export * from './ng2-cesiumjs.module'
+export { cesiumExpRoutes } from './ng2-cesiumjs-routing.module'
 
 // all components that will be codegen'd need to be exported for AOT to work
 export * from './src/billboard/billboard.component'
--- ng2-cesiumjs-routing.module.ts
+++ ng2-cesiumjs-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BillboardComponent } from './src/billboard/billboard.component'
 
-const cesiumExpRoutes: Routes = [
+export const cesiumExpRoutes: Routes = [
     { path: 'billboard', component: BillboardComponent }
 ]
 @NgModule({
